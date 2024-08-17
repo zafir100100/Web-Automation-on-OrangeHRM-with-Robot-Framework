09df2f1bdb17fdc863fef6bebd59deee4080c2a5
Zafir <55599023+zafir100100@users.noreply.github.com>
2024-08-17 13:38:52 +0600

    parallel execution script ready

---
+++ runUI_parallel.bat
@@ -0,0 +1,17 @@
+:: ---------------------------------------------------------------------------------------------------------------------
+ECHO Starting Automation Tests...
+:: ---------------------------------------------------------------------------------------------------------------------
+:: INFO: To install the required libraries
+:: pip install -r requirements.txt
+:: ---------------------------------------------------------------------------------------------------------------------
+:: INFO: To execute robot scripts locally
+call pabot ^
+  --processes 4 ^
+  --outputdir Results ^
+  --variable Environment:sandbox ^
+  --variable BROWSER:headlesschrome ^
+  --variable BROWSER_WIDTH:1440 ^
+  --variable BROWSER_HEIGHT:1024 ^
+  --loglevel TRACE ^
+  --include Regression ^
+  TestCases/*.robot
